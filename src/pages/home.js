d71d1c4aa5934a6a978846fdde3437c274131d59
cecibasbora <105225682+cecibasbora@users.noreply.github.com>
2024-07-15 16:25:02 -0300

    Card update

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,23 @@
 import React from "react";
+import Card from '@mui/joy/Card';
 import "../styles/home.css"; 
 
 function Home() {
   return (
     <div className="home">
-      <h1>Tela inicial (teste)</h1>
+      <div className="telaInit">
+        
+      </div>
+      <div className="manifesto">
+        <h4>Manifesto Lorem Ipsum</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in lacus vel felis molestie commodo. Maecenas vehicula vestibulum tellus, ut finibus mi tempor varius. Phasellus sed semper erat, a condimentum eros. Proin porta massa nulla, a consectetur lorem posuere vel. Nullam finibus id mi ac facilisis.</p>
+      </div>
+      <div className="carrossel"> 
+          <Card/>
+      </div>
+      <div className="patrocinadores">
+
+      </div>
     </div>
   );
 }
